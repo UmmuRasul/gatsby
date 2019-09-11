01f9a88b49c6c0ad618918ceaddbe33d68bef49f
Ummu Rasul <ummurasul5@gmail.com>
2019-09-11 16:19:04 +0300

    css module

---
+++ src/pages/about-css-modules.js
@@ -0,0 +1,11 @@
+import React from "react"
+import styles from "./about-css-modules.module.css"
+import Container from "../components/container"
+
+console.log(styles)
+export default () => (
+  <Container>
+    <h1>About Mariam</h1>
+    <p>Mariam CSS Modules are cool</p>
+  </Container>
+)
